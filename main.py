932ee3d5f53ab0078e6fcb06191e7434f47bad61
nandobfer <nandobfer@yahoo.com.br>
2022-11-09 10:24:45 -0300

    feat: added setlimit

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ def estimatedTotal(bw_per_day):
 def getSolvedLimit(bw_final):
     limit = bw_final * 100 / 75
     return limit
-    
+
+def setLimit(user, value):
+    proc = subprocess.Popen([f"whmapi1 user={user} bwlimit={value} --output=json"], stdout=subprocess.PIPE, shell=True)
+    print()
+    (out, err) = proc.communicate()
+    print(out)
+
 def getQuota(user):
     proc = subprocess.Popen([f"whmapi1 showbw searchtype=user search={user} year 2022 month 11 --output=json"], stdout=subprocess.PIPE, shell=True)
     print()
@@ -40,6 +46,7 @@ def getQuota(user):
     print(f'bandwidth per day: {formated_size(size(per_day))}')
     print(f'estimated bandwidth at months end: {formated_size(size(estimated_total))}')
     print(f'recomended limit: {formated_size(size(new_limit))}')
+    setLimit(user, size(new_limit))
     
 if __name__ == "__main__":
     user = sys.argv[1]
