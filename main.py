2ad3a622c7d07821ec3185675d90f40a3ce6e023
nandobfer <nandobfer@yahoo.com.br>
2022-11-09 09:36:57 -0300

    update: system args

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ def getQuota(user):
     print()
     (out, err) = proc.communicate()
     out = json.loads(str(out))
-    out = out.split('/dev/sda1')[1]
-    out = out.strip()
-    used = int(out.split(' ')[0])
-    limit = int(out.split(' ')[2])
-    print(f'user: {user}')
-    print(f'used space: {used}')
-    print(f"limit: {limit}")
+    print(out)
+    # out = out.split('/dev/sda1')[1]
+    # out = out.strip()
+    # used = int(out.split(' ')[0])
+    # limit = int(out.split(' ')[2])
+    # print(f'user: {user}')
+    # print(f'used space: {used}')
+    # print(f"limit: {limit}")
     
 if __name__ == "__main__":
-    print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
+    user = sys.argv[1]
+    getQuota(user)
 else:
     proc = subprocess.Popen(['ls /home'], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
