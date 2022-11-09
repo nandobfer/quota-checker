01ca5af4719e10c1542d90cd7fe8acec45a4f0bd
nandobfer <nandobfer@yahoo.com.br>
2022-11-09 08:34:14 -0300

    feat: added getquota

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import os
-test = os.system('quota neurokidsclinica')
-print('teste:')
-print(test)
+import subprocess
+
+
+def getQuota(user):
+        proc = subprocess.Popen([f"quota {user}"], stdout=subprocess.PIPE, shell=True)
+        print()
+        (out, err) = proc.communicate()
+        out = str(out)
+        out = out.split('/dev/sda1')[1]
+        out = out.strip()
+        used = int(out.split(' ')[0])
+        limit = int(out.split(' ')[2])
+        print(f'user: {user}')
+        print(f'used space: {used}')
+        print(f"limit: {limit}")
+
+proc = subprocess.Popen(['ls /home'], stdout=subprocess.PIPE, shell=True)
+(out, err) = proc.communicate()
+out = str(out)
+out = out.split('\\n')
+out.pop(0)
+out.pop(-1)
+
+for user in out:
+        try:
+                getQuota(user)
+        except:
+                pass
