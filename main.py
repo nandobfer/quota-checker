c41ff0baec4f56a41999468bbd15b813df9b79bf
nandobfer <nandobfer@yahoo.com.br>
2022-11-09 09:44:52 -0300

    updated: formated output

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ def getQuota(user):
     proc = subprocess.Popen([f"whmapi1 showbw searchtype=user search={user} year 2022 month 11 --output=json"], stdout=subprocess.PIPE, shell=True)
     print()
     (out, err) = proc.communicate()
-    out = json.loads(str(out)[2:-1])
-    print(out)
-    print(type(out))
-    # out = out.split('/dev/sda1')[1]
-    # out = out.strip()
-    # used = int(out.split(' ')[0])
-    # limit = int(out.split(' ')[2])
-    # print(f'user: {user}')
-    # print(f'used space: {used}')
-    # print(f"limit: {limit}")
+    out = json.loads(str(out)[2:-1])['data']['acct']
+    used = int(out['totalbytes'])
+    limit = int(out['limit'])
+    print(f'user: {user}')
+    print(f'used space: {used}')
+    print(f"limit: {limit}")
     
 if __name__ == "__main__":
     user = sys.argv[1]
