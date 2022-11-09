845cadc95ea90c1b651889f16c3edbe28d893b36
nandobfer <nandobfer@yahoo.com.br>
2022-11-09 10:51:12 -0300

    update: dynamic date

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def setLimit(user, value):
         print(f'failed to update {user} limit')
 
 def getQuota(user):
-    proc = subprocess.Popen([f"whmapi1 showbw searchtype=user search={user} year 2022 month 11 --output=json"], stdout=subprocess.PIPE, shell=True)
+    proc = subprocess.Popen([f"whmapi1 showbw searchtype=user search={user} year {now.year} month {now.month} --output=json"], stdout=subprocess.PIPE, shell=True)
     print()
     (out, err) = proc.communicate()
     out = json.loads(str(out)[2:-1])['data']['acct'][0]
@@ -54,16 +54,16 @@ def getQuota(user):
 if __name__ == "__main__":
     user = sys.argv[1]
     getQuota(user)
-else:
-    proc = subprocess.Popen(['ls /home'], stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    out = str(out)
-    out = out.split('\\n')
-    out.pop(0)
-    out.pop(-1)
+# else:
+#     proc = subprocess.Popen(['ls /home'], stdout=subprocess.PIPE, shell=True)
+#     (out, err) = proc.communicate()
+#     out = str(out)
+#     out = out.split('\\n')
+#     out.pop(0)
+#     out.pop(-1)
 
-    for user in out:
-        try:
-            getQuota(user)
-        except:
-            pass
+#     for user in out:
+#         try:
+#             getQuota(user)
+#         except:
+#             pass
