8e4ff2f53ed2e64ee650d8598131bc72115992b2
nandobfer <nandobfer@yahoo.com.br>
2022-11-09 10:14:30 -0300

    test: output

--- main.py
+++ main.py
@@ -4,8 +4,33 @@ from datetime import datetime
 
 now = datetime.now()
 
-def size(value):
-    return f'{value / 1024} mb'
+# bytes pretty-printing
+UNITS_MAPPING = [
+    (1<<50, ' PB'),
+    (1<<40, ' TB'),
+    (1<<30, ' GB'),
+    (1<<20, ' MB'),
+    (1<<10, ' KB'),
+    (1, (' byte', ' bytes')),
+]
+
+
+def size(bytes, units=UNITS_MAPPING):
+    """Get human-readable file sizes.
+    simplified version of https://pypi.python.org/pypi/hurry.filesize/
+    """
+    for factor, suffix in units:
+        if bytes >= factor:
+            break
+    amount = int(bytes / factor)
+
+    if isinstance(suffix, tuple):
+        singular, multiple = suffix
+        if amount == 1:
+            suffix = singular
+        else:
+            suffix = multiple
+    return str(amount) + suffix
 
 def perDay(bw):
     bw_per_day = bw / now.day
